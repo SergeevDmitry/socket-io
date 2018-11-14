a38ab71c83fd9e68c2f6eb88a65cf2455ee1cb6e
sergeevdms <sergeevdms@gmail.com>
2018-11-14 16:14:00 +0300

    added socket io emit

--- server/server.js
+++ server/server.js
@@ -15,6 +15,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('new user connected');
 
+  socket.emit('newEmail', {
+    from: 'mike@example.com',
+    text: 'Hello how are you?',
+    createAt: 12312
+  });
+
+  socket.on('createEmail', (newEmail) => {
+    console.log('createEmail', newEmail);
+  })
+
   socket.on('disconnect', () => {
     console.log('User was Disconnected');
   })
