9a5946d8118cf6e23c101b736ae97a21d29c7d32
sergeevdms <sergeevdms@gmail.com>
2018-11-14 16:43:58 +0300

    added message emits

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const http = require('http');
 const express = require('express');
-
 const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
@@ -15,19 +14,24 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('new user connected');
 
-  socket.emit('newEmail', {
-    from: 'mike@example.com',
+  socket.emit('newMessage', {
+    from: 'Mike',
     text: 'Hello how are you?',
     createAt: 12312
   });
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    })
   });
 
   socket.on('disconnect', () => {
     console.log('User was Disconnected');
-  })
+  });
 });
 
 server.listen(port, () => {
